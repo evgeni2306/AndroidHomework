1cf558feec9e55cedf334f029f36fc8af8add763
Evgeni2306 <Evgeni2306@outlook.com>
2022-04-11 16:47:52 +0500

    1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,23 +1,43 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-    ReceiverFragment receiverFragment;
-  SenderFragment senderFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        receiverFragment = new ReceiverFragment();
-        senderFragment =  new SenderFragment();
+
+        FloatingActionButton Call = findViewById(R.id.call_button);
+        Call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Абонент не доступен", Toast.LENGTH_SHORT).show();
+            }
+        });
+        FloatingActionButton Message = findViewById(R.id.message_button);
+        Message.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Абонент не доступен", Toast.LENGTH_SHORT).show();
+            }
+        });
+        FloatingActionButton Videocall = findViewById(R.id.videocall_button);
+        Videocall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Абонент не доступен", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
